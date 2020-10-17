33dd9f9c3c4c32ab8623d3d541f006391aae0396
sofiisi <sofiariverar@gmail.com>
2020-10-17 10:36:14 -0300

    Update main.js

--- rgbcolorslider-master/dist/js/main.js
+++ rgbcolorslider-master/dist/js/main.js
@@ -189,13 +189,12 @@ function colorSliders(){
         g2 = green.value;
        b2 = blue.value;
     var r1 = parseInt(r2, 10);
-    var r5 = parseInt(red.value, 10);
-   // int g1 = parseInt(g2);
-   // int b1 = parseInt(b2);
+    var g1 = parseInt(g2, 10);
+    var b1 = parseInt(b2, 10);
+
           console.log("updating");
           console.log(red.value);
           console.log(r1);
-          console.log(r5);
 
 
             ref.update({
